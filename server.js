2815db71c6968eb44631f8bdaf824394f7dfa38b
sharvarikhedkar <anujakhedkar96@gmail.com>
2020-07-26 16:05:29 -0700

    register issue solved

--- server.js
+++ server.js
@@ -53,11 +53,11 @@ app.get('/',(req,res)=> {
 });
 
 app.post('/signin',(req,res) => {
+
     if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
         res.json(database.users[0]);
-        //res.json('success');
-    }else{
-        res.status(400).json("error logging in");
+    } else {
+        res.status(400).json('error logging in');
     }
 }
 )
@@ -66,7 +66,6 @@ app.post('/register', (req,res) => {
     const { email, password, name} = req.body;
     bcrypt.hash(password, saltRounds, function(err, hash) {
         // Store hash in your password DB.
-        //console.log(hash);
     });
     database.users.push({
         id:'125',
@@ -76,7 +75,7 @@ app.post('/register', (req,res) => {
         entries: 0,
         joined: new Date()
         })
-    res.json(database).users[database.users.length-1];
+    res.json(database.users[database.users.length-1]);
 })
 
 app.get('/profile/:id',(req,res) => {
