1e3940e65314890cb175e08993857b3ffa168e79
sharvarikhedkar <sharvari996@gmail.com>
2020-07-26 13:35:32 -0700

    adding routes

--- server.js
+++ server.js
@@ -1,14 +1,92 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-
-app.get('/',(req,res)=> {
-    res.send("this is working bro!");
-});
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log('app is running on port 3000');
 });
+
+const database = {
+    users:[
+        {
+        id:'123',
+        name:'john',
+        email: 'john@gmail.com',
+        password: 'cookies',
+        entries: 0,
+        joined: new Date()
+        },
+        {
+            id:'124',
+            name:'sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/',(req,res)=> {
+    res.send(database);
+});
+
+app.post('/signin',(req,res) => {
+    if(req.body.email === database.users[0].email && req.body.password === database.users[0].password){
+        res.json("Logged in");
+    }else{
+        res.status(400).json("Error logging in");
+    }
+    //res.json("signin is working")
+}
+)
+
+app.post('/register', (req,res) => {
+    const { email, password, name} = req.body;
+    database.users.push({
+        id:'125',
+        name: name,
+        email: email,
+        password: password,
+        entries: 0,
+        joined: new Date()
+        })
+    res.json(database);
+})
+
+app.get('/profile/:id',(req,res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user =>{
+        if(user.id === id){
+            found = true;
+            return res.json(user)
+        }
+        }
+    )
+    if(!found){
+        res.status(400).json("User Not Found");
+    }
+    //res.json("signin is working")
+})
+
+app.put('/image',(req,res) =>{
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user =>{
+        if(user.id === id){
+            found = true;
+            user.entries = user.entries + 1;
+            return res.json(user.entries);
+        }
+        }
+    )
+    if(!found){
+        res.status(400).json("User Not Found");
+    }
+})
 
 /*
 / --> res = this is working
