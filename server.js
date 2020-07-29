f0a2f405dda5131b8af98b63047cb19128dfdb5c
sharvarikhedkar <sharvari996@gmail.com>
2020-07-28 23:57:38 -0700

    user profile and image route db connection

--- server.js
+++ server.js
@@ -104,34 +104,27 @@ app.post('/register', (req,res) => {
 
 app.get('/profile/:id',(req,res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user =>{
-        if(user.id === id){
-            found = true;
-            return res.json(user)
+    db.select('*').from('users').where({ id }).then(user => {
+
+        if (user.length){
+        res.json(user[0])
         }
+        else{
+            res.status(400).json('Not Found');
         }
-    )
-    if(!found){
-        res.status(400).json("User Not Found");
-    }
-    //res.json("signin is working")
-})
+    })
+    .catch(err => res.status(400).json("not found"));
+}))
 
 app.put('/image',(req,res) =>{
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user =>{
-        if(user.id === id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-        }
-    )
-    if(!found){
-        res.status(400).json("User Not Found");
-    }
+    db('users').where('id', '=', id)
+    .increment('entries',1)
+    .returning(entries)
+    .then(entries =>{
+        res.json(entries[0])
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
 })
 
 /*
