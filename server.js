8d76456c9e7072c4fa30b6eedf7066bbe8670eb9
sharvarikhedkar <sharvari996@gmail.com>
2020-08-03 19:00:24 -0700

    adding port

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.post('/register',(req,res) => { register.handleRegister(req,res,db,bcrypt,sa
 app.get('/profile/:id',(req,res) => { profile.handleProfile(req,res,db)})
 app.put('/image',(req,res) =>{ image.handleImage(req,res,db)})
 app.post('/imageurl',(req,res) =>{ image.handleApiCall(req,res)})
-app.listen(rpcess.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}}`) });
+app.listen(process.env.PORT || 3000, () => { console.log(`app is running on port ${process.env.PORT}}`) });
 
 /*
 / --> res = this is working
