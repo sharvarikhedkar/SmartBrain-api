e3b30840f554cbfec64ee6ea66f4bb606f21e335
Mandar Kulkarni <mandar242@gmail.com>
2020-08-03 17:15:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ const cors = require('cors');
 // using npm package "knex.js" to connect our postgres db to server
 const knex = require('knex');
 const { response } = require('express');
+const register = require('./controllers/register.js');
+const signin = require('./controllers/signin.js');
+const profile = require('./controllers/profile.js');
+const image = require('./controllers/image.js');
+
 
 const db = knex({
     client: 'pg',
@@ -62,88 +67,13 @@ app.use(bodyParser.json());
 // }
 
 //Routes for application
-app.get('/',(req,res)=> {
-    res.send(database);
-});
+app.get('/',(req,res)=> { res.send(database) });
+app.post('/signin',(req,res) => { signin.handleSignin(req,res,db,bcrypt,saltRounds)});
+app.post('/register',(req,res) => { register.handleRegister(req,res,db,bcrypt,saltRounds)});
+app.get('/profile/:id',(req,res) => { profile.handleProfile(req,res,db)})
+app.put('/image',(req,res) =>{ image.handleImage(req,res,db)})
+app.listen(3000, () => { console.log('app is running on port 3000') });
 
-app.post('/signin',(req,res) => {
-    db.select('email','hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data =>{
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid){
-                return db.select('*').from('users')
-                    .where('email','=',req.body.email)
-                    .then(user => {
-                        res.json(user[0]);
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            }else{
-            res.status(400).json('wrong username or password');
-            }
-        })
-        .catch(err => res.status(400).json('wrong username or password'));
-})
-
-app.post('/register', (req,res) => {
-    const { email, password, name} = req.body;
-    const hash = bcrypt.hashSync(password,saltRounds);
-
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-            return trx('users')
-            .returning('*')
-            .insert({
-                email:loginEmail[0],
-                name: name,
-                joined: new Date()
-            }) 
-            .then(user => {
-                res.json(user[0]);
-        })
-        })
-        .then(trx.commit)
-        .catch(trx.rollback)
-    })
-    .catch(err => res.status(400).json('unable to register'));
-})
-
-app.get('/profile/:id',(req,res) => {
-    const { id } = req.params;
-    
-    db.select('*').from('users').where({ id }).then(user => {
-
-        if (user.length){
-        res.json(user[0])
-        }
-        else{
-            res.status(400).json('Not Found');
-        }
-    })
-    .catch(err => res.status(400).json("not found"));
-})
-
-app.put('/image',(req,res) =>{
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then(entries =>{
-        res.json(entries[0]);
-    })
-    .catch(err => res.status(400).json('unable to get entries'));
-})
-
-
-app.listen(3000, () => {
-    console.log('app is running on port 3000');
-});
 /*
 / --> res = this is working
 /signing --> POST = success/fail
