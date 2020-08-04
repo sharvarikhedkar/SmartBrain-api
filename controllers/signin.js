ee8b1317255ce90e0e0ed191b466381298417beb
Mandar Kulkarni <mandar242@gmail.com>
2020-08-03 17:15:55 -0700

    Add files via upload

---
+++ controllers/signin.js
@@ -0,0 +1,22 @@
+const handleSignin = (req,res,db,bcrypt,saltRounds) => {
+    db.select('email','hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data =>{
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if (isValid){
+                return db.select('*').from('users')
+                    .where('email','=',req.body.email)
+                    .then(user => {
+                        res.json(user[0]);
+                    })
+                    .catch(err => res.status(400).json('unable to get user'))
+            }else{
+            res.status(400).json('wrong username or password');
+            }
+        })
+        .catch(err => res.status(400).json('wrong username or password'));
+}
+
+module.exports ={
+    handleSignin: handleSignin
+}
